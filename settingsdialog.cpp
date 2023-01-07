443d89f7eb7a589c84e255ef3969dc71b594b173
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-01-07 21:10:46 +0200

    Open dialog to select MAME hash file directory

--- settingsdialog.cpp
+++ settingsdialog.cpp
@@ -3,6 +3,7 @@
 #include <QLineEdit>
 #include <QFormLayout>
 #include <QHBoxLayout>
+#include <QFileDialog>
 
 SettingsDialog::SettingsDialog(QWidget *parent)
     : QDialog(parent)
@@ -15,7 +16,15 @@ SettingsDialog::SettingsDialog(QWidget *parent)
     mamePathLayout->addWidget(buttonInfo);
     formLayout->addRow(tr("Path to MAME hash files folder"), mamePathLayout);
     setLayout(formLayout);
+    connect(buttonInfo, SIGNAL(clicked()), this, SLOT(slotOpenFileSelector()));
     setWindowTitle(tr("Settings"));
     setMinimumSize(160, 160);
     resize(480,320);
+}
+
+void SettingsDialog::slotOpenFileSelector()
+{
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Open directory with MAME hash files"),
+                "/home", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    // TODO: Signal directory change
 }
--- settingsdialog.h
+++ settingsdialog.h
@@ -10,6 +10,8 @@ class SettingsDialog : public QDialog
     Q_OBJECT
 public:
     SettingsDialog(QWidget *parent = nullptr);
+private slots:
+    void slotOpenFileSelector();
 };
 
 #endif // SETTINGSDIALOG_H
