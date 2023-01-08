5f9f66ade2f39a976b37534304716a7deebeaae1
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-01-08 17:27:02 +0200

    Check that file is readable

--- mamehashfilereader.cpp
+++ mamehashfilereader.cpp
@@ -1,6 +1,7 @@
 #include <QDebug>
 #include <QDir>
 #include <QXmlStreamReader>
+#include <QMessageBox>
 #include "mamehashfilereader.h"
 
 MameHashFileReader::MameHashFileReader(QString hashFilePath, QObject *parent)
@@ -16,6 +17,19 @@ void MameHashFileReader::read()
     QStringList fileNames = directory.entryList(filters);
     for(const auto& fileName : fileNames){
         qInfo() << "Read file " << fileName;
+        QString filePath = directory.filePath(fileName);
+        QFile file(filePath);
+        if (!file.open(QFile::ReadOnly | QFile::Text)) {
+            QMessageBox messageBox;
+            QString messageText = tr("Cannot read file %1:\n%2")
+                    .arg(QDir::toNativeSeparators(filePath), file.errorString());
+
+            messageBox.setText(messageText);
+            messageBox.exec();
+            return;
+        }
+
+
         //QXmlStreamReader xml;
         //xml.setDevice();
     }
